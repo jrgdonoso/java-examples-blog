90d9aae5320be755a2d59c2f6cba717bb3b4a701
jrgdonoso <jrgdonoso@users.noreply.github.com>
2023-05-28 19:14:35 -0400

    Update Demo1Application.java

--- src/main/java/com/example/demo1/Demo1Application.java
+++ src/main/java/com/example/demo1/Demo1Application.java
@@ -18,20 +18,29 @@ public class Demo1Application {
 		cities.add("London");
 		cities.add("Tokyo");
 		cities.add("Cairo");
+		cities.add("Catemu");
 
 		System.out.println("---imprimir utilizando for()---");
 		for (int i = 0; i < cities.size(); i++) {
 			System.out.println("ciudad: " + cities.get(i));
 		}
 
+		/*
+		 * el ptatron de diseño pipeline permite procesar el elemnto y devolverlo para
+		 * que se pueda seguir procesando
+		 * para utilizazar las funciones pipeline se puede anteponer stream()
+		 */
 		System.out.println("---imprimir utilizando forEach()---");
 		System.out.println("----con lamda----");
 		cities.stream().forEach(city -> System.out.println(city));
 		System.out.println("----con referencia ametodo----");
 		cities.stream().forEach(System.out::println);
-		//cities.forEach(Main::printCty); //referencia al metodo local
-		System.out.println("----Con parallet----");
-		cities.stream().parallel().forEach(System.out::println);
+		// cities.forEach(Main::printCty); //referencia al metodo local
+		System.out.println("----Con parallet----"); // los procesa por bloque, de esta forma no itera los elementos en
+													// orden
+		cities.stream().parallel().forEach(System.out::println); // desde stream()
+		System.out.println("----pipeline Filtro----");
+		cities.stream().filter(city -> city.toLowerCase().startsWith("c")).forEach(System.out::println);
 
 	}
 
